36a944f1de8d0ce3104bab7985769b1967a1f141
Peredude <pereofc@gmail.com>
2026-04-09 03:02:30 +0530

    Update mega_download.py

--- bot/helper/mirror_leech_utils/download_utils/mega_download.py
+++ bot/helper/mirror_leech_utils/download_utils/mega_download.py
@@ -3,8 +3,4 @@ from ...listeners.mega_listener import MegaAppListener
 
 async def add_mega_download(listener, path):
     mega_listener = MegaAppListener(listener)
-
-    if hasattr(listener, "links") and listener.multi > 1:
-        await mega_listener.download_multiple(listener.links, path)
-    else:
-        await mega_listener.download(path)
+    await mega_listener.download(path)
